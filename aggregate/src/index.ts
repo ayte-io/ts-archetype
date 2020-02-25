cab2bc45dd5322c929d8398220b2c39f37fb9da8
Etki <etki@ayte.team>
2020-02-25 13:54:23 +0300

    Release: 0.2.4

--- aggregate/src/index.ts
+++ aggregate/src/index.ts
@@ -2,3 +2,4 @@ export * from '@ayte/archetype.core';
 export * from '@ayte/archetype.collection';
 export * from '@ayte/archetype.function';
 export * from '@ayte/archetype.value';
+export * from '@ayte/archetype.validation';
